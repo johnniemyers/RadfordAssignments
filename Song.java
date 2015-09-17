f0e735fbb09a7f21528c729d7562e3aaaddf6f4e
Johnnie Myers <myers.johnnie3536@gmail.com>
2015-09-17 10:26:04 -0400

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,20 @@
+/**
+* Song.java
+*
+*
+* @author Johnnie Myers
+* @version 31-AUG-2015
+*/
+public class Song
+{
+	public static void main (String[] args)
+	{
+		System.out.println("\nRow!\n");
+		System.out.println("\nRow\n");
+		System.out.println("\nRow Your Boat\n");
+		System.out.print("\nGently Down the Stream\n");
+		System.out.print("\nMerrily Merrily Merrily Merrily\n");
+		System.out.print("\nLife is but a dream!\n");
+		System.out.println("\nThe addition to the print command ln will automatically print it on a new line where as with print you must specify a new line using a command such as the \n command \n");
+	}
+}
