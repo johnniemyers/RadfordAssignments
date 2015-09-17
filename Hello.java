2bde4be5c04ae5f2d34ded2ac55a33ba12796682
Johnnie Myers <myers.johnnie3536@gmail.com>
2015-09-17 10:19:40 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/**
+* Hello.java
+*
+*
+* @author Johnnie Myers
+* @version 31-AUG-2015
+*/
+public class Hello
+{
+	public static void main (String[] args)
+	{
+		System.out.println("\nHello World!\n");
+	}
+}
