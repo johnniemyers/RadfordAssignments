407287e15aa1040c6940ec43fc07319bd6692e9d
Johnnie Myers <myers.johnnie3536@gmail.com>
2015-09-17 11:24:55 -0400

    Create RandomTest.java

---
+++ RandomTest.java
@@ -0,0 +1,59 @@
+/** 
+Relay.java
+
+Author:Johnnie Myers
+Original Shawn Brenneman
+Version 2015-Sep-1
+*/
+import java.util.Random;
+public class RandomTest
+{
+	public static void main (String[] args)
+	{
+	Random RandomTest = new Random();
+	final int MAX = 4;
+	int ranNum = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum + ". Congratulations!");
+	int ranNum1 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum1 + ". Congratulations!");
+	int ranNum2 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum2 + ". Congratulations!");
+	int ranNum3 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum3 + ". Congratulations!");
+	int ranNum4 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum4 + ". Congratulations!");
+	int ranNum5 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum5 + ". Congratulations!");
+	int ranNum6 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum6 + ". Congratulations!");
+	int ranNum7 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum7 + ". Congratulations!");
+	int ranNum8 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum8 + ". Congratulations!");
+	int ranNum9 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum9 + ". Congratulations!");
+	int ranNum10 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum10 + ". Congratulations!");
+	int ranNum11 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum11 + ". Congratulations!");
+	int ranNum12 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum12 + ". Congratulations!");
+	int ranNum13 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum13 + ". Congratulations!");
+	int ranNum14 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum14 + ". Congratulations!");
+	int ranNum15 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum15 + ". Congratulations!");
+	int ranNum16 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum16 + ". Congratulations!");
+	int ranNum17 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum17 + ". Congratulations!");
+	int ranNum18 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum18 + ". Congratulations!");
+	int ranNum19 = RandomTest.nextInt(MAX);
+	System.out.println("You rolled a " + ranNum19 + ". Congratulations!");
+	}
+}
+
+/* 
+*/
